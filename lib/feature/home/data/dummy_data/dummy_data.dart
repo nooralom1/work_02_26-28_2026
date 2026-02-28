44fba64f043250d3eb459a76f9cab23275c53a6a
Md Noor-Alom <non377938@gmail.com>
2026-02-28 11:59:23 +0600

    updated

--- lib/feature/home/data/dummy_data/dummy_data.dart
+++ lib/feature/home/data/dummy_data/dummy_data.dart
@@ -1,18 +1,12 @@
 ///////--------- Home Screen Slider Image Url List -----------/////
 List<String> sliderImages = [
-  "https://img.lazcdn.com/us/domino/ddf2fd28-1f77-4f64-a835-c604a0ead328_BD-1976-688.jpg_2200x2200q80.jpg_.avif",
-  "https://img.lazcdn.com/us/domino/5c57e4a0-653d-4fd7-8c97-e9f2be78ab39_BD-1976-688.jpg_2200x2200q80.jpg_.avif",
-  "https://img.lazcdn.com/us/domino/c0a6dad0-dd04-45ed-976d-ac7bc9772c73_BD-1976-688.jpg_2200x2200q80.jpg_.avif",
-  "https://img.lazcdn.com/us/domino/7ecb5f58-6828-4acc-9499-8150964e961a_BD-1976-688.jpg_2200x2200q80.jpg_.avif",
-  "https://img.lazcdn.com/us/domino/5e41eccd-9a9b-4382-ade7-1c200caf2d89_BD-1976-688.jpg_2200x2200q80.jpg_.avif",
   "https://img.lazcdn.com/us/domino/947e8cb4-f12c-45be-9c47-c3dff4a6bf0f_BD-1976-688.jpg_2200x2200q80.jpg_.avif",
-  "https://img.lazcdn.com/us/domino/c01197ea-7783-4f67-bd27-916914096f42_BD-1976-688.jpg_2200x2200q80.jpg_.avif",
-  "https://img.lazcdn.com/us/domino/96cd8ecd-c1e6-42bc-b721-9bf5fdcda8a4_BD-1976-688.jpg_2200x2200q80.jpg_.avif",
-  "https://img.lazcdn.com/us/domino/08f42997-6ea3-41b4-946b-e897dcbb4dc6_BD-1976-688.jpg_2200x2200q80.jpg_.avif",
-  "https://img.lazcdn.com/us/domino/d99a3afc-785e-45b2-9125-9a9607bbe4f9_BD-1976-688.jpg_2200x2200q80.jpg_.avif",
-  "https://img.lazcdn.com/us/domino/02920c00-4ec2-415f-a71b-7865d6eed869_BD-1976-688.jpg_2200x2200q80.jpg_.avif",
+  "https://img.lazcdn.com/us/domino/8bdd25bc-0c38-4e38-86e0-6e3bb5db8d1f_BD-1976-688.jpg_2200x2200q80.jpg_.avif",
+  "https://img.lazcdn.com/us/domino/7b982ec0-e769-4ba2-ada1-bb5f9ac37bb0_BD-1976-688.jpg_2200x2200q80.jpg_.avif",
+  "https://img.lazcdn.com/us/domino/034b3b0a-72c2-4841-8285-800b2b579bf3_BD-1976-688.jpg_2200x2200q80.jpg_.avif",
+  "https://img.lazcdn.com/us/domino/ae31632b-9747-4db1-b4dc-250d7efbf788_BD-1976-688.jpg_2200x2200q80.jpg_.avif",
 ];
 
 ///////--------- Home Screen Grand Image Url -----------/////
 String grandImage =
-    "https://img.lazcdn.com/us/domino/0a575696-176e-4d80-b84f-aeeb57bb2162_BD-1188-140.gif_2200x2200q80.gif_.webp";
+    "https://img.lazcdn.com/us/domino/07a2a040-da9e-4585-9443-708444569772_BD-1188-140.gif_2200x2200q80.gif_.webp";
